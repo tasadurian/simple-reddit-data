2098cbb8da4a4cee77eddb4fb03053b44b7827f3
Thomas Asadurian <Tasadurian@gmail.com>
2016-02-21 19:11:03 -0800

    added get function

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 var request = require('request-promise');
 
-var Reddit = (function() {
-    function Reddit {};
+getJSON = function(url) {
+  var options = {
+    url: url,
+    json: true,
+  };
+  return rp.get(options)
+    .catch(function(error) {
+      return error;
+    })
+    .then(function(response) {
+      return response;
+    });
+};
 
+var Reddit = (function() {
+
+  function Reddit() {}
+
+  Reddit.prototype.get = function(subreddit) {
+    if (!subreddit) {
+      return getJSON('https://www.reddit.com/.json');
+    } else {
+      return getJSON('https://www.reddit.com/r/' + subreddit + '/.json');
+    }
+  };
+
+  return Reddit;
 })();
 
 module.exports = Reddit;
